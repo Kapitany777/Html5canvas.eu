14f46965d74665051b3f3f24f1e05ef65f0c7573
Kapitany777 <torokv@vnet.hu>
2018-06-21 10:45:47 +0200

    First commit

---
+++ page.php
@@ -0,0 +1,56 @@
+<?php
+
+function PageHeader()
+{
+    print("<div id=\"header\">\n");
+    print("    <div id=\"mainLogo\">\n");
+    print("        <a href=\"index.php\">\n");
+    print("            <img src=\"images/HTML5_Logo_64.png\" alt=\"logo\" width=\"64\" height=\"64\">\n");
+    print("        </a>\n");
+    print("    </div>\n");
+            
+    print("    <div id=\"mainTitle\">\n");
+    print("        <a href=\"index.php\">HTML5Canvas.eu</a>\n");
+    print("    </div>\n");
+            
+    print("    <div id=\"mainMenu\">\n");
+    print("        <ul>\n");
+    print("            <li><a href=\"demok.php\">Demók</a></li>\n");
+    print("            <li><a href=\"konyvek.php\">Könyvek</a></li>\n");
+    print("            <li><a href=\"linkek.php\">Linkek</a></li>\n");
+    print("            <li><a href=\"impresszum.php\">Impresszum</a></li>\n");
+    print("        </ul>\n");
+                
+    print("        <img src=\"images/HTML5_3D_Effects_64.png\" alt=\"canvas\" width=\"64\" height=\"64\">\n");
+    print("    </div>\n");
+    print("</div>\n");
+}
+
+function PageH1($h1Text)
+{
+    print("<div id=\"pageHeader\">\n");
+    print("    <h1>" . $h1Text . "</h1>\n");
+    print("</div>\n");
+}
+
+function PageFooter()
+{
+    print("<div id=\"footer\">\n");
+    print("    <div id=\"copyright\">\n");
+    print("        <a href=\"http://prog.lidercfeny.hu/\" target=\"_blank\">&copy; 2001-2018 Tumuslak Vorvahes Team</a>\n");
+    print("    </div>\n");
+            
+    print("    <div id=\"impresszum\">\n");
+    print("        <a href=\"impresszum.php\">Impresszum</a>\n");
+    print("    </div>\n");
+    print("</div>\n");
+}
+
+function Back()
+{
+    print("<p>\n");
+    print("     <a href=\"javascript:history.back();\">Előző oldal</a>\n");
+    print("</p>\n");
+}
+
+?>
